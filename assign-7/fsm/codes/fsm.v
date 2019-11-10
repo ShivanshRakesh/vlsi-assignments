83dcf50b3e73bf61e5c4da7e8758a52c27885c93
ShivanshRakesh <49039969+ShivanshRakesh@users.noreply.github.com>
2019-11-10 22:24:41 +0530

    added assignment-7

--- assign-7/fsm/codes/fsm.v
+++ assign-7/fsm/codes/fsm.v
@@ -1,87 +1,57 @@
-module fsm(CLK, RESET, INP, OUT);
-    input CLK, RESET, INP;
-    output reg OUT;
-    reg [2:0] state;
+module fsm(CLK, RESET, ONE, ZERO, OUT);
+    input CLK, RESET, ONE, ZERO;
+    output OUT;
+    reg [3:0] state=0;
+    reg prev0=0, prev1=0;
+		
+    always @(negedge ZERO) begin prev0 <= 0; end
+    always @(negedge ONE) begin prev1 <= 0; end
+    
+	assign OUT = (state == 8);
 	 
-    always @(posedge CLK or posedge RESET) begin
+    always @(posedge CLK) begin
         if (RESET == 1) begin
             state <= 0;
-            OUT <= 0;
+            prev0 <= 0;
+            prev1 <= 0;
         end
-        else begin 
-            // STATE 00
-            if (state == 0) begin
-                if (INP == 0) begin 
-                    state <= 1;
-                end
-                else begin
-                    state <= 3;
-                end
-            end
-            
-            // STATE 01
-            if (state == 1) begin
-                if (INP == 0) begin 
-                    state <= 1;
-                end
-                else begin
-                    state <= 2;
-                end
-            end
-            
-            // STATE 10
-            if (state == 2) begin
-                if (INP == 0) begin 
-                    state <= 5;
-                end
-                else begin
-                    state <= 2;
-                end
-            end
-
-            // STATE 11-1
-            if (state == 3) begin
-                if (INP == 0) begin
-                    state <= 4;
-                end
-                else begin 
-                    state <= 3;
-                end
-            end
-
-            // STATE 11-0
-            if (state == 4) begin
-                if (INP == 0) begin
-                    state <= 4;
-                end
-                else begin 
-                    state <= 6;
-                end
-            end
-
-            // STATE 12
-            if (state == 5) begin
-                if (INP == 0) begin
-                    state <= 5;
-                end
-                else begin 
-                    state <= 7;
-                    OUT <= 1;
-                end
-            end
-            
-            // STATE 21
-            if (state == 6) begin
-                if (INP == 0) begin
-                    state <= 7;
-                    OUT <= 1;
-                end
-                else begin
-                    state <= 6;                 
-                end
-            end   
-        end
-    end
+        else begin
+            case (state)
+				0 : begin 
+						if (~prev0 & ZERO) begin prev0 <= 1; state <= 1; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 2; end
+					 end
+				1 : begin 
+						if (~prev0 & ZERO) begin prev0 <= 1; state <= 3; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 4; end
+					 end
+				2 : begin 
+						if (~prev0 & ZERO) begin prev0 <= 1; state <= 4; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 5; end
+					 end
+				3 : begin 
+						if (~prev0 & ZERO) begin prev0 <= 1; state <= 3; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 6; end
+					 end
+			   4 : begin 
+						if (~prev0 & ZERO) begin prev0 <= 1; state <= 6; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 7; end
+					 end
+				5 : begin 
+						if (~prev0 & ZERO) begin prev0 <= 1; state <= 7; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 5; end
+				    end
+				6 : begin 
+						if (~prev0 & ZERO) begin prev0 <= 1; state <= 6; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 8; end
+					 end
+            7 : begin 
+					   if (~prev0 & ZERO) begin prev0 <= 1; state <= 8; end
+						else if (~prev1 & ONE) begin prev1 <= 1; state <= 7; end
+					 end
+				endcase
+			end
+    end    
 endmodule
 
 
--- assign-7/fsm/codes/fsm_tb.v
+++ assign-7/fsm/codes/fsm_tb.v
@@ -4,9 +4,9 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   17:53:39 11/09/2019
+// Create Date:   16:52:00 11/10/2019
 // Design Name:   fsm
-// Module Name:   /home/shivansh/verilog/verilog-proj/fsm_tb.v
+// Module Name:   /home/shivansh/verilog/verilog-proj/fsmtb1.v
 // Project Name:  verilog-proj
 // Target Device:  
 // Tool versions:  
@@ -22,12 +22,13 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module fsm_tb;
+module fsmtb1;
 
 	// Inputs
 	reg CLK;
 	reg RESET;
-	reg INP;
+	reg ONE;
+	reg ZERO;
 
 	// Outputs
 	wire OUT;
@@ -36,81 +37,75 @@ module fsm_tb;
 	fsm uut (
 		.CLK(CLK), 
 		.RESET(RESET), 
-		.INP(INP), 
+		.ONE(ONE), 
+		.ZERO(ZERO), 
 		.OUT(OUT)
 	);
-	
-	always #1 CLK = ~CLK;
+
 	initial begin
 		// Initialize Inputs
 		CLK = 0;
 		RESET = 1;
-		INP = 0;
+		ONE = 0;
+		ZERO = 0;
 
 		// Wait 100 ns for global reset to finish
 		#10;
-		
+		RESET = 0;
+        
 		// Add stimulus here
-		#2
-		RESET = 0;
 		#2;
-		INP = 1;
-		#2; 
-		INP = 0;
-		#2; 
-		INP = 0;
-		#2; 
-		INP = 0;	
-		#2; 
-		INP = 1;
-		#10;
-		RESET = 1;
-		
-		#2
-		RESET = 0;
+		ONE = 1;
 		#2;
-		INP = 1;
-		#2; 
-		INP = 0;
-		#2; 
-		INP = 1;
-		#2; 
-		INP = 0;	
-		#2; 
-		INP = 1;
-		#10;
-		RESET = 1;
-		
-		#2
-		RESET = 0;
+		ONE = 0;
+		ZERO = 1;
 		#2;
-		INP = 1;
-		#2; 
-		INP = 0;
-		#2; 
-		INP = 0;
-		#2; 
-		INP = 0;	
-		#2; 
-		INP = 0;
-		#10;
-		RESET = 1;
-		
-		#2
-		RESET = 0;
+		ZERO = 0;
+		ONE = 1;
 		#2;
-		INP = 1;
-		#2; 
-		INP = 1;
-		#2; 
-		INP = 0;
-		#2; 
-		INP = 0;	
-		#2; 
-		INP = 0;
-		#10;
+		ZERO =1;
+		ONE = 0;
+		#2;
+		ZERO = 0;
+		#2;
 		RESET = 1;
+		#2;
+		RESET = 0;
 		
-   end   
+		#2;
+		ONE = 0;
+		#2;
+		ZERO = 1;
+		#2;
+		ZERO = 0;
+		#2;
+		ZERO = 1;
+		#2;
+		ZERO = 0;
+		#2;
+		ZERO = 1;
+		#2;
+		ZERO = 0;
+		#2;
+		ZERO = 1;
+
+		#2;
+		ONE = 0;
+		#2;
+		ONE = 1;
+		#2;
+		ONE = 0;
+		#2;
+		ONE = 1;
+		#2;
+		ONE = 0;
+		#2;
+		ONE = 1;
+		#2;
+		ONE = 0;
+
+	end
+	always #1 CLK = ~CLK;
+      
 endmodule
 
